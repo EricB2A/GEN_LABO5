a8d714d00df4aab8caee11c79c33fa6914c87b7e
Eric Broutba <eric.bousbaa@hotmail.com>
2020-06-15 20:06:21 +0200

    bad smell: primitive obsession, refactor: replace Type Code with Subclasses (cf 57c15d9)

--- sources/Movie.h
+++ sources/Movie.h
@@ -6,10 +6,6 @@
 
 class Movie {
 public:
-    static const int CHILDRENS   = 2;
-    static const int REGULAR     = 0;
-    static const int NEW_RELEASE = 1;
-
     Movie( const std::string& title, Price* priceCode = new PriceRegular() );
 
     Price* getPriceCode() const;
