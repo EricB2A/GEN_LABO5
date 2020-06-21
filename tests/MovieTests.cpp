88edd69535017029a0a4bcc878c5d8a59ec6e58f
Eric Broutba <eric.bousbaa@hotmail.com>
2020-06-21 14:08:27 +0200

    Dans les tests, Movie devient MockingMovie

--- tests/MovieTests.cpp
+++ tests/MovieTests.cpp
@@ -3,34 +3,39 @@
 #include "../sources/Customer.h"
 #include "../sources/prices/PriceNewRelease.h"
 #include "../sources/prices/PriceChildren.h"
+#include "mock/MockingMovie.cpp"
+#include "mock/MockingRental.cpp"
 
 TEST(MovieTests, testTitle)
 {
     const std::string TITLE = "The Room";
-    Movie movie(TITLE);
-    ASSERT_EQ(TITLE, movie.getTitle());
+    MockingMovie mockingMovie(TITLE);
+    ASSERT_EQ(TITLE, mockingMovie.getTitle());
 }
 
 TEST(MovieTests, testRegularPrice)
 {
     const std::string TITLE = "Troll 2";
-    Movie movie(TITLE);
-    ASSERT_EQ(6.5, movie.getPriceCode()->priceByType(5));
-    ASSERT_EQ(2, movie.getPriceCode()->priceByType(2));
+    MockingMovie mockingMovie(TITLE);
+    ASSERT_EQ(6.5, mockingMovie.getPriceCode()->priceByType(5));
+    ASSERT_EQ(2, mockingMovie.getPriceCode()->priceByType(2));
 }
 
 TEST(MovieTests, testChildrenPrice)
 {
     const std::string TITLE = "Dünyayı Kurtaran Adam";
-    Movie movie(TITLE, PriceChildren::getInstance());
-    ASSERT_EQ(12, movie.getPriceCode()->priceByType(10));
-    ASSERT_EQ(1.5, movie.getPriceCode()->priceByType(3));
+    MockingMovie mockingMovie(TITLE, PriceChildren::getInstance());
+    ASSERT_EQ(12, mockingMovie.getPriceCode()->priceByType(10));
+    ASSERT_EQ(1.5, mockingMovie.getPriceCode()->priceByType(3));
 }
 
 TEST(MovieTests, testNewReleasePrice)
 {
     const std::string TITLE = "Plan 9 from Outer Space";
-    Movie movie(TITLE, PriceNewRelease::getInstance());
-    ASSERT_EQ(3, movie.getPriceCode()->priceByType(1));
-    ASSERT_EQ(15, movie.getPriceCode()->priceByType(5));
+    MockingMovie mockingMovie(TITLE, PriceNewRelease::getInstance());
+    ASSERT_EQ(3, mockingMovie.getPriceCode()->priceByType(1));
+    ASSERT_EQ(15, mockingMovie.getPriceCode()->priceByType(5));
 }
+
+
+
